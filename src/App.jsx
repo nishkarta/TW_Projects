12fa4388f2545e61dceb4bf18aded96756f4ed7d
nishkarta <nishkarta@gmail.com>
2022-11-22 15:33:00 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}>
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainComp.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const MainComp = () => {
+    const [movies, setMovies] = React.useState([])
+
+    return (
+        <div>MainComp</div>
+    )
+}
+
+export default MainComp
